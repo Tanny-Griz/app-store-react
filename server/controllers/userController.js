6fe05506fa5bfb8b9bbed64ab53760c4f4b48523
Tanya <tanya.griz89@gmail.com>
2023-03-11 19:06:50 -0600

    initialization project, started added server pert

---
+++ server/controllers/userController.js
@@ -0,0 +1,19 @@
+const ApiError = require("../error/ApiError")
+
+class UserController {
+  async registration(req, res) {
+
+  }
+  async loin(req, res) {
+
+  }
+  async check(req, res, next) {
+    const {id} = req.query
+    if(!id) {
+      return next(ApiError.badRequest('No set'))
+    }
+    res.json(id)
+  }
+}
+
+module.exports = new UserController()
