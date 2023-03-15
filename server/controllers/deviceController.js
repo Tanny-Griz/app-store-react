1e53768c3816e446d2b706d566167e04779b42cb
Tanya <tanya.griz89@gmail.com>
2023-03-15 11:33:35 -0500

    added token auth

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -10,7 +10,7 @@ class DeviceController {
             const {img} = req.files
             let fileName = uuid.v4() + ".jpg"
             img.mv(path.resolve(__dirname, '..', 'static', fileName))
-            const device = await Device.create({name, price, brandId, typeId, img: fileName});
+            const device = await Device.create({name, price, brandId, typeId, img: fileName})
 
             if (info) {
                 info = JSON.parse(info)
