0b5fdfddd49ea736b15a368b6aa540ac14f7707d
Tanya <tanya.griz89@gmail.com>
2023-03-15 19:34:15 -0500

    added router changes

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,19 +1,24 @@
-import React from "react"
+import React, { useContext } from "react"
 import {Routes, Route, Navigate} from 'react-router-dom'
+import { Context } from ".."
 import { authRoutes, publicRoutes } from "../routes"
 import {SHOP_ROUTE} from "../utils/consts"
 
 const AppRouter = () => {
-  const isAuth = false
+  const {user} = useContext(Context)
+  console.log('user', user)
   return (
     <Routes>
-      {isAuth && authRoutes.map(({path, Component}) =>
+      {user.isAuth && authRoutes.map(({path, Component}) =>
         <Route key={path} path={path} element={ <Component/> } exact />
       )}
       {publicRoutes.map(({path, Component}) =>
         <Route key={path} path={path} element={ <Component/> } exact />
       )}
-      <Route path="/" element={<Navigate replace to={SHOP_ROUTE} />} />
+      <Route
+        path="*"
+        element={<Navigate to={SHOP_ROUTE} replace />}
+      />
     </Routes>
   )
 }
